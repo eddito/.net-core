ce7e826752d69cab3741f60c1e50ad09206e141f
li <li@DESKTOP-7952FEM>
2019-03-23 16:29:06 +0800

    初始化

---
+++ Tcm/Models/Results.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tcm.Models
+{
+    public class Results
+    {
+        public String Message { get; set; }
+    }
+}
